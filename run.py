4bb251af573f9b405d7e61c74628b10dca61975a
llm-wait <58540759+llm-wait@users.noreply.github.com>
2020-12-01 13:15:01 +0800

    Update run.py

--- run.py
+++ run.py
@@ -16,4 +16,3 @@ report_now=time.strftime('%Y-%m-%d_%H_%M_%S')
 with open(test_report_parh+"/result_"+report_now+".html",'wb')as f:
     runner=HTMLTestRunner(stream=f, title='系统接口',description='系统接口测试')
     runner.run(suit)
-#jekins密码51bee3607a5b4b25b211256ebe402e66
